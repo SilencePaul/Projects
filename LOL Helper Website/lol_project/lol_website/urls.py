024803fe6bec10d2640b2b48e4fc7175f570b601
SilencePaul <94564922+SilencePaul@users.noreply.github.com>
2023-12-26 21:06:17 -0500

    champion detail page updates

--- LOL Helper Website/lol_project/lol_website/urls.py
+++ LOL Helper Website/lol_project/lol_website/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('champions/', views.champions, name='champions'),
+    re_path(r'^champions/(?P<champion_name>[a-zA-Z]+)/$', views.champion_detail, name='champion_detail'),
 ]
--- LOL Helper Website/lol_project/lol_website/views.py
+++ LOL Helper Website/lol_project/lol_website/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Champion
+import json
+
+from .models import Champion, ChampionDetail
 
 def index(request):
     context = {}
@@ -26,3 +28,32 @@ def champions(request):
     context["images"] = images
     context["champions"] = champions
     return render(request, 'champions.html', context)
+
+def champion_detail(request, champion_name):
+    context = {}
+    champion = ChampionDetail.objects.get(champion__search_name=champion_name)
+    spells = json.loads(champion.spells)
+    passive = json.loads(champion.passive)
+    stats = json.loads(champion.stats)
+    skins = json.loads(champion.skins)
+    allytips = json.loads(champion.allytips)
+    enemytips = json.loads(champion.enemytips)
+    partytype = champion.partype
+    lore = champion.lore
+
+    allytips = [tip.replace("<br>", "") for tip in allytips]
+    enemytips = [tip.replace("<br>", "") for tip in enemytips]
+    skin_list = []
+    skin_url = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + champion.champion.search_name + "_"
+    for skin in skins:
+        skin_list.append([skin["name"], skin_url + str(skin["num"]) + ".jpg"])
+
+    context["champion"] = champion
+    context["skins"] = skin_list
+    context["spells"] = spells
+    context["passive"] = passive
+    context["allytips"] = allytips
+    context["enemytips"] = enemytips
+    context["lore"] = lore
+    context["partytype"] = partytype
+    return render(request, 'champion_detail.html', context)
