34405e8a2e28281fa9f865dc99fb09b182d7bef3
SilencePaul <94564922+SilencePaul@users.noreply.github.com>
2024-04-16 09:16:59 -0400

    reset to normal

--- LOL Helper Website/lol_project/lol_project/settings.py
+++ LOL Helper Website/lol_project/lol_project/settings.py
@@ -11,24 +11,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os  # Import os to use environment variables
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jqf5#0_26on2gyh!)n2^ya(rx5(3=e14q+ufqt$_c0zrf6dg*w'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-very-unsafe-key')
+DEBUG = False
+ALLOWED_HOSTS = ['159.203.6.155', 'localhost']
 
 INSTALLED_APPS = [
     'lol_website.apps.LolWebsiteConfig',
@@ -70,20 +59,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'lol_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -100,26 +81,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'America/Toronto'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- LOL Helper Website/lol_project/lol_website/apps.py
+++ LOL Helper Website/lol_project/lol_website/apps.py
@@ -1,24 +1,8 @@
 from django.apps import AppConfig
-from apscheduler.schedulers.background import BackgroundScheduler
-import os
-
-def test_working():
-    print("working")
-
-def start():
-    from .tasks import get_summoner_info
-    get_summoner_info("supermanman")
-    get_summoner_info("BiaoGe")
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(get_summoner_info, 'interval', minutes=20, args=["supermanman"])
-    scheduler.add_job(get_summoner_info, 'interval', minutes=20, args=["BiaoGe"])
-    scheduler.start()
 
 class LolWebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lol_website'
 
     def ready(self):
-        if os.environ.get('RUN_MAIN') == 'true':
-            print("Starting scheduler...")
-            start()
+        pass
