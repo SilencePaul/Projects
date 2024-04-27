428c7829a97ed4a0d57a64951851e01a17079778
SilencePaul <94564922+SilencePaul@users.noreply.github.com>
2024-04-26 22:21:46 -0400

    Remove lol_helper_website directory

--- lol_helper_website/lol_project/lol_website/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class LolWebsiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lol_website'
-
-    def ready(self):
-        pass
--- lol_helper_website/lol_project/lol_website/management/commands/test_function.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-import requests
-from lol_website.tasks import get_summoner_info
-
-class Command(BaseCommand):
-    help = 'Test function'
-
-    def handle(self, *args, **options):
-        get_summoner_info("supermanman")
